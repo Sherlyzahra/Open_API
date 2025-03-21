f700db3f27258c2e379c35f9b05d97030704885b
Sherlyzahra <sherlyazzahra983@gmail.com>
2025-03-21 17:31:39 +0800

    Membuat API dengan Express

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import http from 'http';
+import express from 'express';
 import mysql from 'mysql2';
 
-const db = mysql.createConnection ({ host: "localhost", user: "root", database: "openapi", password: ""});
+const db = mysql.createConnection({ host: "localhost", user: "root", database: "users", password: ""});
+const app = express();
 
-const server = http.createServerServer((req, res) => {
+app.get('/users', (req, res) => {
     db.query('SELECT * FROM user', (err, results) => {
         if (err) {
-            res.writeHead(500, { 'Content-Type': "text/plain"});
-            res.end('Internal Server Error');
+            res.status(500).send('Internal Server Error');
             return;
         }
 
-        reswriteHead(200, { 'Content-Type': "apllication/json" });
-        res.end(JSON.stringify(results));
+        res.json(results);
     });
-    });
-    server.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
+});
+    app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
